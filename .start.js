cd46b99f99b60b2fe30dbc6be6d2c9e6590dd7b6
Martin Algañaraz <idcmardelplata@gmail.com>
2017-09-29 15:47:05 -0300

    Arreglado el autostart para que funcione en todas las plataformas

---
+++ .start.js
@@ -0,0 +1,12 @@
+const util = require("util");
+const stat = require('fs').stat;
+const exec = require('child_process').exec;
+
+(function install() {
+    stat('./node_modules/', (err, stats) => {
+      if (err) {
+        console.log("Instalando las dependencias, por fabor aguarde....");
+        exec('npm install');
+      }
+    })
+  })();
